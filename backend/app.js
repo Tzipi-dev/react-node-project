0dbc1ba8602747a9b3f9d8aaa0e0445f4bc71c9b
Tzipi-dev <tzipi3135@gmail.com>
2025-03-20 01:49:02 +0200

    mongoDB connected

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,25 @@
-const express=require('express')
-const app=express()
-const PORT=process.env.PORT||5000
-const userRouter=require('./routes/users')
-const lostRouter=require('./routes/losts')
-const foundRouter=require('./routes/founds')
-const mongoose  = require('mongoose')
-require("dotenv").config()
-app.use('/users',userRouter)
-app.use('/losts',lostRouter)
-app.use('/founds',foundRouter)
+const express = require("express")
+const app = express()
+require('dotenv').config();
+const cors=require("cors")
+const userRouter = require('./routes/users')
+const lostRouter = require('./routes/losts')
+const foundRouter = require('./routes/founds')
+const corsOptions=require("./config/corsOptions")
+const connectDB=require("./config/dbConn")
+const PORT = process.env.PORT || 5000
+connectDB()
+const  mongoose = require("mongoose")
 
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.static("public"))
+app.use('/users', userRouter)
+app.use('/losts', lostRouter)
+app.use('/founds', foundRouter)
 
-mongoose.connect(process.env.CONECTION_URL)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-
-
-    
-
-
-
-
-
+mongoose.connect(process.env.CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => app.listen(PORT, () => {
+        console.log(`server is runing on port ${PORT}`);
+    })
+).catch((error) => { console.log(error) })
