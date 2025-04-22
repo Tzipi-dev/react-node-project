ee31a0468d1a0069bc6939819dc49c6aaa243e1a
Tzipi-dev <tzipi3135@gmail.com>
2025-04-22 16:28:21 +0300

    formDesign

--- frontend/src/globalStyle.ts
+++ frontend/src/globalStyle.ts
@@ -4,3 +4,31 @@ export const errorCSS: CSSProperties = {
     color: 'red',
     fontSize: 'smaller'
 }
+export const loginContainer: CSSProperties = {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  };
+  
+  export const loginBox: CSSProperties = {
+    padding: "30px",
+    width: "400px",
+    borderRadius: "15px",
+    boxShadow: "0 0 15px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#ffffff",
+  };
+  
+  export const loginTitle: CSSProperties = {
+    textAlign: "center",
+    marginBottom: "20px",
+    fontSize: "24px",
+    fontWeight: 600,
+  };
+  
+  export const loginForm: CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+  };
