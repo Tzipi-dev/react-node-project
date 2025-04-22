ea89adacf342c86f2e00d42126887e25e77b1cf7
Tzipi-dev <tzipi3135@gmail.com>
2025-04-22 18:20:11 +0300

    uptade form design

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,10 +1,11 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { errorCSS, loginBox, loginContainer, loginForm, loginTitle } from "../globalStyle";
+import { errorCSS, loginBox, loginForm, loginTitle, margin, topbtn } from "../globalStyle";
 import { zodResolver } from "@hookform/resolvers/zod";
 import UserSchema from "../schemas/UserSchema";
 import { useAddUserMutation } from "../redux/api/users/apiUserSlice";
 import { User } from "../interfaces/models";
+import { useNavigate } from "react-router";
 // import { Link } from "react-router";
 
 const SignUp = () => {
@@ -22,28 +23,26 @@ const SignUp = () => {
 
         }
     }
+    const navigate=useNavigate()
+
     return (
         <div>
-            <div style={loginContainer}>
+            <div >
                 <div style={loginBox}>
                     <Typography sx={loginTitle}>SignUp</Typography>
-                    <form style={loginForm} onSubmit={handleSubmit(onSubmit)} noValidate>
-                        <div>שם:</div>
-                        <TextField id="filled-basic" label="שם" variant="filled" {...register("name")} />
-                        {errors.name && <p style={errorCSS}>{errors.name.message}</p>}
-                        <div>מייל:</div>
-                        <TextField id="filled-basic" label="מייל" variant="filled" {...register("email")} />
-                        {errors.email && <p style={errorCSS}>{errors.email.message}</p>}
-                        <div>טלפון:</div>
-                        <TextField id="filled-basic" label="טלפון" variant="filled" {...register("phone")} />
-                        {errors.phone && <p style={errorCSS}>{errors.phone.message}</p>}
-                        <div>סיסמה:</div>
-                        <TextField id="filled-basic" label="סיסמה" variant="filled" type="password" {...register("password")} />
-                        {errors.password && <p style={errorCSS}>{errors.password.message}</p>}
-                        <div> <Button variant="outlined" type="submit" >sign up</Button></div>
-                        <Button variant="contained" color="primary" type="submit" fullWidth>
-                            SignUp
-                        </Button>
+                    <form style={loginForm} onSubmit={handleSubmit(onSubmit)} >
+                        <TextField id="filled-basic" label="שם" variant="filled" {...register("name")} style={margin} />
+                        {errors.name && <div style={errorCSS}>{errors.name.message}</div>}
+                        <TextField id="filled-basic" label="מייל" variant="filled" {...register("email")} style={margin}/>
+                        {errors.email && <div style={errorCSS}>{errors.email.message}</div>}
+                        <TextField id="filled-basic" label="טלפון" variant="filled" {...register("phone")}style={margin} />
+                        {errors.phone && <div style={errorCSS}>{errors.phone.message}</div>}
+                        <TextField id="filled-basic" label="סיסמה" variant="filled" type="password" {...register("password")} style={margin}/>
+                        {errors.password && <div style={errorCSS}>{errors.password.message}</div>}
+                        <div> 
+                        <Button variant="contained" color="primary" type="submit" fullWidth style={topbtn}>sign up</Button>
+                        <Button variant="outlined" color="primary" fullWidth onClick={()=>{navigate('/')}}>ביטול</Button>
+                        </div>
                     </form>
                 </div>
             </div>
