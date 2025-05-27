123bcb85c6894fc7d8259ded7a42a145711ba012
Tzipi-dev <tzipi3135@gmail.com>
2025-05-27 16:02:18 +0300

    Remove unwanted folder

--- react-node-project/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const UserDetails=new mongoose.Schema({
-    name: String,
-    password: String,
-    phone: String,
-    email: String,
-    
-})
-module.exports=mongoose.model('User',UserDetails)
--- react-node-project/backend/routes/foundsByIdUser.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const verifyJWT =require('../middlewares/verifyJWT')
-const { getFoundsByIdOwner}=require("../controllers/founds")
-
-router.get('/:id',getFoundsByIdOwner)
-
-
-module.exports=router
